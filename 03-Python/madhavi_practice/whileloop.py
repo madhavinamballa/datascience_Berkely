d9d6bfc90b35054e76602f11f27bbfe8ee1afd09
Madhavinamballa <madhavi@Madhavinamballas-MacBook-Pro.local>
2020-09-08 11:28:19 -0700

    added python folder

---
+++ 03-Python/madhavi_practice/whileloop.py
@@ -0,0 +1,15 @@
+i=0
+while i<10:
+    print(i)
+    i += 1
+
+#for loop
+print("this is from loop result")
+for i in range(10):
+    print(i)
+#decremental while loop
+print("this is from decremental while loop")
+j=10
+while j>0:
+    print(j)
+    j -= 1
